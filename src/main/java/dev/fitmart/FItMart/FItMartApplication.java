c5028ce2b381bba64b2068c2e4d66a5facc07d96
HienQuach <hien.quach.131296@gmail.com>
2025-04-26 18:30:39 +0700

    first code

---
+++ src/main/java/dev/fitmart/FItMart/FItMartApplication.java
@@ -0,0 +1,11 @@
+package dev.fitmart.FItMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FItMartApplication {
+
+	public static void main(String[] args) {SpringApplication.run(FItMartApplication.class, args);}
+
+}
